393b51a619e042a9bfd00c7ebdccceb5d149ed0d
Lado Asambadze <lado.asambadze1@gmail.com>
2023-12-02 21:38:57 +0400

    feat: define types for house page

--- app/house/page.tsx
+++ app/house/page.tsx
@@ -3,9 +3,22 @@
 import axios from "axios";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
+interface ImageType {
+  original: string;
+}
+interface HouseType {
+  room: number;
+  price: number;
+  district: {
+    title: string;
+  };
+  images: ImageType[];
+}
 export default function House() {
-  const [house, setHouse] = useState([]);
+  const [house, setHouse] = useState<HouseType[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   let size = 5;
 
@@ -24,22 +37,38 @@ export default function House() {
 
   const totalPages = Math.ceil(house.length / size);
 
-  const changePage = (newPage) => {
+  const changePage = (newPage: number) => {
     setCurrentPage(newPage);
   };
 
   return (
-    <div className="w-full  min-h-screen  grid  grid-col-1 gap-6  items-center px-[10%] sm:px-[15%] py-3 bg-[#eee8e8]  md:grid-cols-2 ">
+    <div className="w-full  min-h-screen  grid  grid-col-1 gap-6 justify-center px-[10%]  py-5 bg-[#eee8e8]  md:grid-cols-2 ">
       {currentItems &&
         currentItems.map((item, index) => (
-          <div className="w-full bg-white h-[320px] rounded-md flex flex-col rounded-2 overflow-hidden shadow-2xl shadow-#7f7878-400">
-            <div className="w-full h-[65%] relative" key={index}>
-              <Image
-                src={item.images[0].original}
-                alt="Banner"
-                layout="fill"
-                objectFit="cover"
-              />
+          <div
+            className="w-[350px] bg-white h-[300px] rounded-md flex flex-col rounded-2 overflow-hidden shadow-2xl shadow-#7f7878-400
+          md:w-full"
+          >
+            <div className="w-full h-[75%] relative" key={index}>
+              <Carousel responsive={responsive} infinite={true}>
+                {item.images.map((image, i) => (
+                  <div
+                    style={{
+                      position: "relative",
+                      width: "100%",
+                      height: "200px",
+                    }}
+                  >
+                    <Image
+                      key={i}
+                      src={image.original}
+                      alt="House image"
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                ))}
+              </Carousel>
             </div>
             <div className="flex flex-row items-center px-3 py-3 border-b-2 border-[gray]">
               <span className=" text-[black]">
@@ -48,7 +77,7 @@ export default function House() {
             </div>
             <div className="flex flex-row items-center justify-between px-3 py-3 ">
               <span>Location: {item.district.title} </span>
-              <span className=" text-[black]">{item.price}</span>
+              <span className=" text-[black]">{item.price} total rent</span>
             </div>
           </div>
         ))}
@@ -60,3 +89,22 @@ export default function House() {
     </div>
   );
 }
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 4000, min: 1440 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1440, min: 768 },
+    items: 1,
+  },
+  tabletOne: {
+    breakpoint: { max: 768, min: 430 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 430, min: 0 },
+    items: 1,
+  },
+};
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        xsm: "450px", // Add your custom breakpoint here
+      },
+    },
+  },
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
