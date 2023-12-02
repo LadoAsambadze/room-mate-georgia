735231949ac2a45e3dfd5ee98f735e6930189a5e
Lado Asambadze <lado.asambadze1@gmail.com>
2023-12-02 12:10:39 +0400

    feat: create test template to fetch data and output for test

--- app/house/[houseID]/page.tsx
+++ app/house/[houseID]/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function HouseID() {
-  return <div>dymainc</div>;
+  return <div className="w-full h-min-h-screen flex flex-col items-center px-3"></div>
 }
--- app/house/page.tsx
+++ app/house/page.tsx
@@ -1,14 +1,36 @@
-import Link from "next/link";
-import React from "react";
+"use client";
+
+import axios from "axios";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
 export default function House() {
-  const id = "1";
+  const [house, setHouse] = useState([]);
+  console.log(house);
+  console.log(house);
+  useEffect(() => {
+    const getHouse = async () => {
+      const response = await axios.get("https://api.roommategeorgia.ge/flats");
+      setHouse(response.data.data);
+    };
+    getHouse();
+  }, []);
   return (
-    <div>
-      <h1>House</h1>
-      <Link href={`/house/${id}`}>
-        <p>Go to House 1</p>
-      </Link>
+    <div className="w-full  min-h-screen flex flex-col items-center px-[15%] py-3 bg-[gray] ">
+      
+      <div className="w-full bg-white h-[320px] rounded-md flex flex-col ">
+        <div className="w-full h-[60%] relative">
+          <Image
+            src="/images/banner.png"
+            alt="Banner"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+
+        <span className=" text-[black]">Number of room</span>
+        <span className=" text-[black]">price</span>
+      </div>
     </div>
   );
 }
